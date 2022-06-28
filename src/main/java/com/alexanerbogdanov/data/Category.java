e33e09ed4633da60f642d0a594c3015237ebb1d4
alexanderbogdanovgit config --global user.name alexanderbogdanovgit config --global user.name alexanderbogdanov <ironweed@list.ru>
2022-06-28 20:45:39 +0300

    Initial commit

---
+++ src/main/java/com/alexanerbogdanov/data/Category.java
@@ -0,0 +1,25 @@
+package com.alexanerbogdanov.data;
+
+import java.util.Arrays;
+
+public enum Category {
+  SHOP("Магазины"), FUEL("АЗС"), BAR("Бары");
+  private final String description;
+
+
+  Category(String description) {
+    this.description = description;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public static Category findCategory(String description) {
+    return Arrays.stream(values())
+            .filter(category -> category.getDescription().equals(description))
+            .findFirst()
+            .orElseThrow();
+
+  }
+}
